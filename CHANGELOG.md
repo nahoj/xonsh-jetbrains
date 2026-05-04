81a5ac4feea4a67a41cf854c9c6daa6144ea47f5
Johan Grande <nahoj+git@crans.org>
2026-05-04 21:40:34 +0200

    Add CHANGELOG.md and Gradle plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Unreleased
+
+## 1.1.1 - 2026-05-04
+
+### Added
+
+- Project icon
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 
 plugins {
     id("java")
+    id("org.jetbrains.changelog") version "2.5.0"
     id("org.jetbrains.kotlin.jvm") version "2.3.21"
     id("org.jetbrains.intellij.platform") version "2.16.0"
 }
